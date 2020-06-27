2abb1b8c2dd72c30f8cc27d7c33953dabbc346ac
Bohdan Khmara <Khmarabohdan@gmail.com>
2020-06-27 14:44:06 +0300

    Added age verification function

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {number} age check
+ * @returns {boolean}
+ */
+function isAdult(age) {
+    if (age < 20) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+isAdult(19);
+
+
